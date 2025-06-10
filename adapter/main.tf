f3126d35848e214fd763697cb90823d62c88e0e6
AlbeCamp21 <luis.alanya.c@uni.pe>
2025-06-10 17:35:25 -0500

    feat(tf): (Issue #4) Modificar adapter/main.tf para que esté correctamente formateado

--- adapter/main.tf
+++ adapter/main.tf
@@ -1,9 +1,9 @@
 resource "null_resource" "adapter_exec" {
-#se crea un recurso nulo dentro de Terraform.
+  #se crea un recurso nulo dentro de Terraform.
   provisioner "local-exec" {
-# especifica que se va a ejecutar un comando local en la maquina de Terraform
-    command = "python3 ${path.module}/adapter_output.py > adapter_output.json" 
-# Ejecuta el script Python, dentro del módulo actual (${path.module}
+    # especifica que se va a ejecutar un comando local en la maquina de Terraform
+    command = "python3 ${path.module}/adapter_output.py > adapter_output.json"
+    # Ejecuta el script Python, dentro del módulo actual (${path.module}
 
   }
 
