abea185220a4db4644c8742ed7184ea060dc97a2
BiancaMT957 <merchantorresbianca75@gmail.com>
2025-06-13 22:32:39 -0400

    feat(sh): (Issue #23) agregar recibir_message.sh con la finalidad de leer mensaje del mediator

--- cliente_b/receive_message.sh
+++ cliente_b/receive_message.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+# Script para recibir el mensaje reenviado por el Mediator
+
+INPUT_FILE="../mediator/message_b.txt"
+
+if [ ! -f "$INPUT_FILE" ]; then
+echo "[cliente_b] ERROR: No se encontró $INPUT_FILE."
+exit 1
+fi
+
+echo "[cliente_b] Mensaje recibido de $INPUT_FILE:"
+cat "$INPUT_FILE"
