61327746f3e8b0c857acad337479e369bc4a5cd0
Jharvy <jharvy.cadillo.t@uni.pe>
2025-06-15 13:06:58 -0500

    feat(py): (Issue #19) refactorizacion del script generar_dependencies.py para la generacion de dependencias dinamicas

--- generar_dependencies.py
+++ generar_dependencies.py
@@ -1,16 +1,44 @@
 import json
+import os
+import re
 
 
 # Crea dependencies.json con contenido estatico
 def generar_dependencies():
     dependencies = {
         "adapter": [],
-        "facade": ["adapter"],
-        "mediator": ["adapter", "facade"]
+        "facade": [],
+        "mediator": []
     }
+
+    for modulo in ["adapter", "facade", "mediator"]:
+        dependencies[modulo] = obtener_dependencias(modulo)
+
+    print("Archivo dependencies.json generado correctamente.")
 
     with open('dependencies.json', 'w') as f:
         json.dump(dependencies, f, indent=4)
+
+
+def obtener_dependencias(module) -> list:
+    dependencias = []
+
+    patrones = [
+        r'module\s*"([^"]+)"',              # module "nombre"
+        r'depends_on\s*=\s*\[([^\]]+)\]',   # depends_on = [x, y, z]
+        r'var\.([a-zA-Z0-9_-]+)',           # var.nombre_variable
+        r'data\.([a-zA-Z0-9_-]+)',          # data.tipo_recurso
+    ]
+
+    for archivo in os.listdir(module):
+        if archivo.endswith("main.tf"):
+            with open(os.path.join(module, archivo), 'r') as f:
+                contenido = f.read()
+                for patron in patrones:
+                    coincidencias = re.findall(patron, contenido)
+                    dependencias.extend(coincidencias)
+
+    return dependencias
 
 
 if __name__ == "__main__":
