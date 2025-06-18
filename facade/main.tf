23076c7028a954f3dbbffcf8a38b08a4657f6296
AlbeCamp21 <luis.alanya.c@uni.pe>
2025-06-18 15:15:46 -0500

    feat(tf): (Issue #37) condicionar el despliegue a adapter_status y agregar variable

--- facade/main.tf
+++ facade/main.tf
@@ -13,7 +13,13 @@ resource "null_resource" "create_file" {
 
 resource "null_resource" "start_service" {
   depends_on = [null_resource.create_file]
+  # Verificador, evita que se despliegue el servicio si Adapter manda un estado diferente de OK.
+  count = var.adapter_status == "OK" ? 1 : 0
   provisioner "local-exec" {
     command = "bash ${path.module}/start_service.sh"
+  }
+  # correr el health check después de iniciar el servicio
+  provisioner "local-exec" {
+    command = "bash ${path.module}/health_check.sh"
   }
 }
