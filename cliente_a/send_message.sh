85d06c567bbb718e4e306eccb8c7bc1f8449617a
AlbeCamp21 <luis.alanya.c@uni.pe>
2025-06-18 23:02:54 -0500

    feat(sh): (Issue #39) agregar validaciones y guardar message_a.txt en facade/facade_dir

--- cliente_a/send_message.sh
+++ cliente_a/send_message.sh
@@ -4,10 +4,22 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 BASE_DIR="$DIR/.."
 
+FACADE_PATH="$BASE_DIR/facade/facade_dir"
+
+# Verifica si la carpeta, donde se almacenara el mensaje, existe
+if [ ! -d "$FACADE_PATH" ]; then
+    echo "[ERROR] Carpeta facade_dir NO existe."
+fi
+
+# Verifica que la variable de entorno CLIENT_A_MSG exista
+if [[ -z "$CLIENT_A_MSG" ]]; then
+    CLIENT_A_MSG="Luis de inspirate"
+fi
+
 # Mensaje por defecto o pasado como argumento
-MSG="${1:-'Hola desde cliente A'}"
+MSG="$CLIENT_A_MSG"
 TIME="$(date -Iseconds)"
-FILE="$BASE_DIR/cliente_a/message_a.txt"
+FILE="$BASE_DIR/facade/facade_dir/message_a.txt"
 
 # Se guarda el mensaje
 echo "{\"msg\": \"$MSG\", \"timestamp\": \"$TIME\"}" > "$FILE"
