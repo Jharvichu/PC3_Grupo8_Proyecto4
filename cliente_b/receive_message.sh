59c742444eb60ba1a4b74593361beb99866e8d5a
AlbeCamp21 <luis.alanya.c@uni.pe>
2025-06-18 23:03:46 -0500

    feat(sh): (Issue #39) añadir funcionalidad de copiar message_b.txt generado por mediator

--- cliente_b/receive_message.sh
+++ cliente_b/receive_message.sh
@@ -5,11 +5,13 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 BASE_DIR="$DIR/.."
 
 INPUT_FILE="$BASE_DIR/mediator/message_b.txt"
+OUTPUT_FILE="$DIR/message_b.txt"
 
 if [ ! -f "$INPUT_FILE" ]; then
 echo "[cliente_b] ERROR: No se encontró $INPUT_FILE."
 exit 1
 fi
 
+cp "$INPUT_FILE" "$OUTPUT_FILE"
 echo "[cliente_b] Mensaje recibido de $INPUT_FILE:"
-cat "$INPUT_FILE"
+cat "$OUTPUT_FILE"
